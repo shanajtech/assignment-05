8c39c0d63f38b98ec286a7269243894f8f7566ad
Shanaj-akter <shanajj764@gmail.com>
2026-09-13 20:04:12 +0600

    Setup React project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col gap-5 items-center justify-center bg-base-200">
+      <h1 className="text-5xl font-bold">
+        Dev Stack Builder
+      </h1>
+
+      <button className="btn btn-primary">
+        Test DaisyUI
+      </button>
+    </div>
+  )
+}
+
+export default App
